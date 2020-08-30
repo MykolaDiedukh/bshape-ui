780f787a68f4c441b83f812340f7d99d206b723e
soul <ykrainec1991@gmail.com>
2020-08-30 12:53:00 +0200

    change carousel and team section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,60 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'bshape-ui';
+  constructor() {
+  }
+  customOptions: OwlOptions = {
+    loop: true,
+    autoplay: true,
+    center: true,
+    dots: false,
+    autoHeight: true,
+    autoWidth: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      400: {
+        items: 2
+      },
+      740: {
+        items: 3
+      },
+      940: {
+        items: 4
+      }
+    },
+  }
+  public slidesData = [{
+    id:1,
+    img: "assets/img/TEAM/pexels-emre-keshavarz-3526923.jpg",
+    name: "Krystian",
+    role: "Ceo",
+    mainRole: "bShape owner.",
+    facebookLink: 'https://www.facebook.com'
+  },{
+    id:2,
+    img: "assets/img/TEAM/pexels-emre-keshavarz-3526923.jpg",
+    name: "Michał",
+    role: "Programista",
+    mainRole: "bShape owner."
+  },{
+    id:3,
+    img: "assets/img/TEAM/pexels-emre-keshavarz-3526923.jpg",
+    name: "Krystian",
+    role: "Ceo",
+    mainRole: "bShape owner."
+  },{
+    id:4,
+    img: "assets/img/TEAM/pexels-emre-keshavarz-3526923.jpg",
+    name: "Krystian",
+    role: "Ceo",
+    mainRole: "bShape owner."
+  },{
+    id:5,
+    img: "assets/img/TEAM/pexels-emre-keshavarz-3526923.jpg",
+    name: "Krystian",
+    role: "Ceo",
+    mainRole: "bShape owner."
+  }];
 }
